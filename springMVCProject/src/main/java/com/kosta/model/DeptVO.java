1d7be2707a3a8527d136c678adce0cdb143da22c
MyGram <753kg@naver.com>
2021-05-11 11:42:50 +0900

    modify DeptVO

--- springMVCProject/src/main/java/com/kosta/model/DeptVO.java
+++ springMVCProject/src/main/java/com/kosta/model/DeptVO.java
@@ -36,7 +36,6 @@ public class DeptVO {
 		this.manager_id = manager_id;
 	}
 	public int getLocation_id() {
-		System.out.println("getLocation_id: " + location_id);
 		return location_id;
 	}
 	public void setLocation_id(int location_id) {
