131e5de55eb4c70df5d3608f33496d13a636f5a6
MyGram <753kg@naver.com>
2021-05-11 17:02:59 +0900

    Mybatis

--- springMVCProject/src/main/java/com/kosta/business/EmpDAOMybatis.java
+++ springMVCProject/src/main/java/com/kosta/business/EmpDAOMybatis.java
@@ -3,102 +3,103 @@ package com.kosta.business;
 import java.sql.Date;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
 import com.kosta.model.EmpVO;
 import com.kosta.model.JobVO;
 
-@Service("empService")
-public class EmpServiceImpl implements EmpService {
+@Repository("empDAO_mybatis")
+public class EmpDAOMybatis implements EmpDAOInterface{
 	
 	@Autowired
-	@Qualifier("empDAO_mybatis")
-	EmpDAOInterface empDAO;
+	SqlSession sqlsession;
+	
+	String namespace = "com.kosta.emp.";
 
 	@Override
 	public EmpVO loginChk(int empid, String email) {
 		// TODO Auto-generated method stub
-		return empDAO.loginChk(empid, email);
+		return null;
 	}
 
 	@Override
 	public List<EmpVO> selectAll() {
-		// TODO Auto-generated method stub
-		return empDAO.selectAll();
+		System.out.println("EmpDAOMybatis...");
+		return sqlsession.selectList(namespace + "selectAll");
 	}
 
 	@Override
 	public List<JobVO> selectAllJobs() {
 		// TODO Auto-generated method stub
-		return empDAO.selectAllJobs();
+		return sqlsession.selectList(namespace + "selectAllJobs");
 	}
 
 	@Override
 	public EmpVO selectById(int empid) {
 		// TODO Auto-generated method stub
-		return empDAO.selectById(empid);
+		return sqlsession.selectOne(namespace + "selectById", empid);
 	}
 
 	@Override
 	public List<EmpVO> selectByDept(int deptid) {
 		// TODO Auto-generated method stub
-		return empDAO.selectByDept(deptid);
+		return sqlsession.selectOne(namespace + "selectByDept", deptid);
 	}
 
 	@Override
 	public List<EmpVO> selectByJob(String jobid) {
 		// TODO Auto-generated method stub
-		return empDAO.selectByJob(jobid);
+		return sqlsession.selectOne(namespace + "selectByJob", jobid);
 	}
 
 	@Override
 	public List<EmpVO> selectBySalary(int minsal, int maxsal) {
 		// TODO Auto-generated method stub
-		return empDAO.selectBySalary(minsal, maxsal);
+		return null;
 	}
 
 	@Override
 	public List<EmpVO> selectByDate(String sdate, String edate) {
 		// TODO Auto-generated method stub
-		return empDAO.selectByDate(sdate, edate);
+		return null;
 	}
 
 	@Override
 	public List<EmpVO> selectByDate2(Date sdate, Date edate) {
 		// TODO Auto-generated method stub
-		return empDAO.selectByDate2(sdate, edate);
+		return null;
 	}
 
 	@Override
 	public List<EmpVO> selectByName(String str) {
 		// TODO Auto-generated method stub
-		return empDAO.selectByName(str);
+		return null;
 	}
 
 	@Override
 	public List<EmpVO> selectByCondition(int deptid, String jobid, int sal, Date hdate) {
 		// TODO Auto-generated method stub
-		return empDAO.selectByCondition(deptid, jobid, sal, hdate);
+		return null;
 	}
 
 	@Override
 	public int insertEmp(EmpVO emp) {
 		// TODO Auto-generated method stub
-		return empDAO.insertEmp(emp);
+		return sqlsession.insert(namespace + "insertEmp", emp);
 	}
 
 	@Override
 	public int updateEmp(EmpVO emp) {
 		// TODO Auto-generated method stub
-		return empDAO.updateEmp(emp);
+		return sqlsession.update(namespace + "updateEmp", emp);
 	}
 
 	@Override
 	public int deleteEmp(int empid) {
 		// TODO Auto-generated method stub
-		return empDAO.deleteEmp(empid);
+		return sqlsession.delete(namespace + "deleteEmp", empid);
 	}
 
 }
