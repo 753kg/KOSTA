bc5d77eb55be0fe6eab227590a5e01b4cc2ee297
MyGram <753kg@naver.com>
2021-07-26 13:35:30 +0900

    백준 설탕배달

--- algorithmStudy/src/step08_기본수학1/No2839.java
+++ algorithmStudy/src/step08_기본수학1/No2839.java
@@ -11,12 +11,17 @@ public class No2839 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
+		// 3kg, 5kg
+		// 18kg --> (X) 3kg x 6개
+		//		--> (O) 5kg x 3개 + 3kg x 1개 ==> 4개
+		
+		// 11kg --> 5kg 1개 3kg 2개
 		int result = 0;
-		if(N % 5 == 0) result = N/5;
+		if(N % 5 == 0) result = N/5; 
 		else {
-			int a = N / 5;
+			int a = N / 5; // 5kg 1개
 			while(true) {
-				int b = N - 5*a;
+				int b = N - 5*a;	// 11kg-5kg =6kg
 				if(b % 3 == 0) {
 					result = a + b/3;
 					break;
