43c8807f0858824fdebf9d7d323c8cd5112419a6
MyGram <753kg@naver.com>
2021-07-27 11:06:51 +0900

    백준 666 문제 이해

---
+++ algorithmStudy/src/step11_브루트포스/No1436.java
@@ -0,0 +1,21 @@
+package step11_브루트포스;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No1436 {
+
+	public static void main(String[] args) throws IOException {
+		// 영화감독 숌
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=N; i++) {
+			
+		}
+
+	}
+
+}
