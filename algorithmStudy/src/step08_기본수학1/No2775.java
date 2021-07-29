1f28a7c1fccf2e93eede24863c630f905cc0deb5
MyGram <753kg@naver.com>
2021-07-29 11:53:20 +0900

    백준 부녀회장

--- algorithmStudy/src/step08_기본수학1/No2775.java
+++ algorithmStudy/src/step08_기본수학1/No2775.java
@@ -1,5 +1,37 @@
 package step08_기본수학1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class No2775 {
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
+		int T = Integer.parseInt(br.readLine());
+		
+		int[][] arr = new int[15][15];	// 0층부터 1~14호
+		
+		for(int j=0; j<15; j++) {
+			arr[0][j] = j;
+			arr[j][1] = 1;
+		}
+		
+		for(int i=1; i<15; i++) {	// 층
+			for(int j=2; j<15; j++) { // 호
+				arr[i][j] = arr[i][j-1] + arr[i-1][j];
+			}
+		}
+		
+		for(int i=0; i<T; i++) {
+			int k = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine());
+			
+			sb.append(arr[k][n]).append("\n");
+		}
+		
+		System.out.println(sb);
+		
+	}
 }
