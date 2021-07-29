ec3fd9636611163eb482bc7944a32972c1c6c159
MyGram <753kg@naver.com>
2021-07-29 11:53:03 +0900

    백준 666

--- algorithmStudy/src/step11_브루트포스/No1436.java
+++ algorithmStudy/src/step11_브루트포스/No1436.java
@@ -3,6 +3,7 @@ package step11_브루트포스;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class No1436 {
 
@@ -12,10 +13,25 @@ public class No1436 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
-		for(int i=1; i<=N; i++) {
+		int num = 666;
+		int count = 1;
+		
+		while(count != N) {
+			num++;
 			
+			if(String.valueOf(num).contains("666")) {
+				count++;
+			}
 		}
-
+		
+		System.out.println(num);
 	}
 
 }
+
+/* 전 숫자가 666이면 1666 or 6661 -> 1666이 없으면 1666, 있으면 6661
+ * 전 숫자가 1666이면 맨앞숫자 + 1
+ * ...
+ * 6666이면 1666 or 6661 -> 1666 있으니까 6661
+ * 전 숫자가 
+*/
